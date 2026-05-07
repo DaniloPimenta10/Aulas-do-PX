c0f54f38eaccd5fdd793a40948183862562c98cd
Danilo Sian <danilopimenta2006@gmail.com>
2026-05-07 18:07:45 -0300

    Ajustes main EP 07

--- EP/EP_07/main.c
+++ EP/EP_07/main.c
@@ -3,27 +3,27 @@
 
 int main()
 {
-  DynVec *dv = dv_create();
-  dv_insert(dv, 10);
-  printf("size=%d capacity=%d\n", dv_size(dv), dv_capacity(dv));
-  dv_print_values(dv);
-  dv_free(dv);
-  dv = dv_create_from_file("data.txt");
-  dv_print_values(dv);
-  printf("size=%d capacity=%d\n", dv_size(dv), dv_capacity(dv));
-  dv_free(dv);
-
   DynVec *dv1 = dv_create();
-  DynVec *dv2 = dv_create();
   dv_insert(dv1, 1);
   dv_insert(dv1, 3);
   dv_insert(dv1, 6);
-  dv_insert(dv2, 6);
-  dv_insert(dv2, 20);
+
+  DynVec *dv2 = dv_create();
   dv_insert(dv2, 3);
+  dv_insert(dv2, 6);
+  dv_insert(dv2, 10);
+  dv_insert(dv2, 20);
 
-  dv = dv_intersection(dv1, dv2);
+  DynVec *v_uniao = dv_union(dv1, dv2);
+  dv_print_values(v_uniao);
 
-  dv_print_values(dv);
+  DynVec *v_inter = dv_intersection(dv1, dv2);
+  dv_print_values(v_inter);
+
+  dv_free(dv1);
+  dv_free(dv2);
+  dv_free(v_uniao);
+  dv_free(v_inter);
+
   return 0;
 }
